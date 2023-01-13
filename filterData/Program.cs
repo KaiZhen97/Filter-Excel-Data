499a5035d13da4406fdb9fada48b3a913ffea1fa
KaiZhen97 <61720239+KaiZhen97@users.noreply.github.com>
2023-01-13 09:19:35 +0800

    Update Program.cs

--- filterData/Program.cs
+++ filterData/Program.cs
@@ -2,13 +2,9 @@
 using System.IO;
 using System.Linq;
 
-using Aspose.Cells; // aspose fast
+using Aspose.Cells;
 using Workbook = Aspose.Cells.Workbook;
 using Worksheet = Aspose.Cells.Worksheet;
-
-//using Spire.Xls.Collections; // spire slow
-//using Workbook = Spire.Xls.Workbook;
-//using Worksheet = Spire.Xls.Worksheet;
 
 namespace filterData
 {
@@ -16,7 +12,6 @@ namespace filterData
     {
         static void Main(string[] args)
         {
-            //Aspose better, faster
             //Create an empty Excel workbook
             //Workbook workbook = new Workbook(@"C:\Users\kaizhen.goh\source\report\CrystalReportViewer-From11Nov2021.xlsx");
             Workbook workbook = new Workbook(@"C:\Users\kaizhen.goh\source\report\CrystalReportViewer1 (6).xlsx");
@@ -47,36 +42,6 @@ namespace filterData
 
             //Save Excel XLSX file
             workbook.Save(@"C:\Users\kaizhen.goh\source\report\DeleteCrystalReportViewerAsposeYN3267H.xlsx");
-            //workbook.Save(@"C:\Users\kaizhen.goh\source\report\FilterCrystalReportViewerAspose.xlsx");
-
-
-            ////spire too slow
-            //// create workbook, load existing file, get first worksheet
-            //Workbook workbook = new Workbook();
-            ////workbook.LoadFromFile(@"C:\Users\kaizhen.goh\source\report\CrystalReportViewer-From11Nov2021.xlsx");
-            //workbook.LoadFromFile(@"C:\Users\kaizhen.goh\source\report\CrystalReportViewer1 (6) - Copy.xlsx");
-            //Worksheet worksheet = workbook.Worksheets[0];
-
-            //// filter
-            //AutoFiltersCollection filters = worksheet.AutoFilters;
-            //filters.Range = worksheet.Range["A1:P15044"];
-            //filters.AddFilter(2, "YN3210X");
-            //filters.Filter();
-
-            //int rowCount = worksheet.Rows.Count();
-
-            //for (int i = 1; i <= rowCount; i++)
-            //{
-            //    if (worksheet.GetRowIsHide(i))
-            //    {
-            //        worksheet.DeleteRow(i);
-            //        i--;
-            //    }
-            //}
-
-            //// save file
-            //workbook.SaveToFile(@"C:\Users\kaizhen.goh\source\report\DeleteCrystalReportViewerSpireYN3210X.xlsx");
-            ////workbook.SaveToFile(@"C:\Users\kaizhen.goh\source\report\FilterCrystalReportViewerSpire.xlsx");
         }
     }
 }
